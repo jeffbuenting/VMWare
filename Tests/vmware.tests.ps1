f8bd7eb3a7677e70a0455030b4451595e00c6120
Jeff Buenting <buenting_jeffrey@bah.com>
2019-11-05 12:49:37 -0500

    moved Get-VMWAREOprphanedFile to separate micro function

--- Tests/vmware.tests.ps1
+++ Tests/vmware.tests.ps1
@@ -1,7 +1,9 @@
 ﻿# ----- Get the module name
 if ( -Not $PSScriptRoot ) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 
-$ModulePath = $PSScriptRoot.substring( 0,$PSSCriptRoot.Length-6 )
+$ModulePath = $PSScriptRoot.trim( '\Tests' )
+
+Write-output "ModulePath = $ModulePath"
 
 $Global:ModuleName = $ModulePath | Split-Path -Leaf
 
@@ -10,7 +12,7 @@ Write-Output "ModuleName = $ModuleName"
 # ----- Remove and then import the module.  This is so any new changes are imported.
 Get-Module -Name $ModuleName -All | Remove-Module -Force
 
-Import-Module "$ModulePath\$ModuleName.PSD1" -Force -ErrorAction Stop 
+Import-Module "$ModulePath\$ModuleName.PSD1" -Force -ErrorAction Stop  
 
 InModuleScope $ModuleName {
 
@@ -21,17 +23,27 @@ InModuleScope $ModuleName {
 
         $Module = Get-module -Name $ModuleName -Verbose
 
-        $testFile = Get-ChildItem $module.ModuleBase -Filter '.\Tests\*.Tests.ps1' -File -verbose
+        $testFile = Get-ChildItem -Path $module.ModuleBase,"$($Module.ModuleBase)\Tests" -Filter '*.Tests.ps1' -File -verbose
     
         $testNames = Select-String -Path $testFile.FullName -Pattern 'Describe "\$ModuleName : (.*)"' | ForEach-Object {
               $_.matches.groups[1].value
         }
-
+ 
         $moduleCommandNames = (Get-Command -Module $ModuleName | where CommandType -ne Alias)
-
+ 
         it 'should have a test for each function' {
             Compare-Object $moduleCommandNames $testNames | where { $_.SideIndicator -eq '<=' } | select inputobject | should beNullOrEmpty
         }
+    }
+
+    #-------------------------------------------------------------------------------------
+    # Call separate function tests
+
+    $PesterResults = @()
+
+    # ----- -exclude was not working so used the where clause
+    Get-ChildItem -path $ModulePath\Tests -Filter *.tests.ps1  | where Name -ne "$ModuleName.Tests.ps1" | foreach {
+        $PesterResults += Invoke-Pester -Script $_.FullName -PassThru
     }
 
     #-------------------------------------------------------------------------------------
@@ -324,226 +336,7 @@ InModuleScope $ModuleName {
 
     Write-Output "`n`n"
 
-    Describe "$ModuleName : Get-VMWareOrphanedFile" -Tags DataStore {
-       
-        
-        
-
-            Mock -Command New-Object -ParameterFilter { $TypeName -eq 'VMware.Vim.FileQueryFlags' } -MockWith {
-                $Obj = @{
-                    FileOwner = $true
-                    FileSize = $true
-                    FileType = $true
-                    Modification = $true 
-                }
     
-                Return $Obj
-            } -Verifiable
-
-            Mock -Command New-Object -ParameterFilter { $TypeName -eq 'VMware.Vim.FloppyImageFileQuery' } -MockWith { Return $Null }
-
-            Mock -Command New-Object -ParameterFilter { $TypeName -eq 'VMware.Vim.FolderFileQuery' } -MockWith {}
-
-            Mock -Command New-Object -ParameterFilter { $TypeName -eq 'VMware.Vim.IsoImageFileQuery' } -MockWith {}
-
-            Mock -Command New-Object -ParameterFilter { $TypeName -eq 'VMware.Vim.VmConfigFileQuery' } -MockWith {
-                Return (@{
-                    Details = $Null
-                })
-            }
-
-            Mock -Command New-Object -ParameterFilter { $TypeName -eq 'VMware.Vim.VmConfigFileQueryFlags' } -MockWith {
-                Return (@{
-                    ConfigVersion = $true
-                }) 
-            }
-
-            Mock -Command New-Object -ParameterFilter { $TypeName -eq 'VMware.Vim.TemplateConfigFileQuery' } -MockWith {
-                Return (@{
-                    Details = $Null
-                })
-            }
-
-            Mock -Command New-Object -ParameterFilter { $TypeName -eq 'VMware.Vim.VmDiskFileQuery' } -MockWith {
-                Return (@{
-                    Details = $Null
-                })
-            }
-
-            Mock -Command New-Object -ParameterFilter { $TypeName -eq 'VMware.Vim.VmDiskFileQueryFlags' } -MockWith {  
-                Return (@{
-                    CapacityKB = $true
-                    DiskExtents = $true
-                    DiskType = $true
-                    HardwareVersion = $true
-                    Thin = $true
-                }) 
-            }
-
-            Mock -Command New-Object -ParameterFilter { $TypeName-eq 'VMware.Vim.VmLogFileQuery' } -MockWith {}
-
-            Mock -Command New-Object -ParameterFilter { $TypeName-eq 'VMware.Vim.VmNvramFileQuery' } -MockWith {}
-
-            Mock -Command New-Object -ParameterFilter { $TypeName-eq 'VMware.Vim.VmSnapshotFileQuery' } -MockWith {}
-
-            Mock -Command New-Object -ParameterFilter { $TypeName-eq 'VMware.Vim.HostDatastoreBrowserSearchSpec' } -MockWith {
-                Return (@{
-                    details = $Null
-                    Query = $Null
-                    sortFoldersFirst = $Null
-                }) 
-            }
-
-            Mock -Command Get-DataStore -MockWith {
-                $MultipleHostAccessObj = (@{
-                    MultipleHostAccess = $True
-                })
-
-                $BrowserObj = (@{
-                    Type = 'HostDatastoreBrowser'
-                    Value = 'datastoreBrowser-datastore-9999'
-                })
-
-                $ExtensionDataObj = (@{
-                    Browser = $BrowserObj
-                    Summary = $MultipleHostAccessObj
-                })
-
-                Return (@{
-                    Type = "VMFS"
-                    ExtensionData = $ExtensionDataObj
-                    ID = $Null
-                    Accessible = $True
-                    Name = "Vol1"
-                }) 
-            }
-
-            Mock -Command Get-View -ParameterFilter { $ID } -MockWith {
-
-                $obj = (@{}) 
-
-                $Obj | Add-Member -MemberType ScriptMethod -Name SearchDatastoreSubFolders -Value {
-                    Param ( $rootPath, $searchSpec )
-
-                    $FileObj = (@{
-                        Path = 'TestDrive:\temp'
-                    })
-
-                    Return (@{
-                        File = $FileObj
-                        FolderPath = 'TestDrive:\temp'
-                    })
-                }
-
-                Return $Obj
-            }
-
-             Mock -Command Get-VM -ParameterFilter { $DataStore } -MockWith {
-
-                $NameObj = (@{
-                    Name = 'TestVM'
-                })
-        
-                $FileObj = (@{
-                    File = $NameObj
-                })
-        
-                $LayoutObj = (@{
-                    Layout = $FileObj
-                })
-  
-                Return (@{
-                    ExtensionData = $LayoutObj
-                }) 
-            }
-
-            Mock -CommandName Get-Template -MockWith {
-
-                $NameObj = (@{
-                    Name = $Null
-                })
-        
-                $FileObj = (@{
-                    File = $NameObj
-                })
-        
-                $LayoutObj = (@{
-                    Layout = $FileObj
-                })
-
-                Return (@{
-                    ExtensionData = $LayoutObj
-                    DatastoreIdList = $Null
-                }) 
-            }
-
-        Context Execution {
-
-            # ----- Currently having issues mocking Get-Vm.  This throws thefollowing error.
-            # ----- the expression not to throw an exception. Message was {Cannot process argument transformation on parameter 'Datastore'. Cannot convert the "System.Collections.Hashtable" value of type "System.Collections.Hashtable" to type "VMware.VimAutomation.ViCore.Types.V1.DatastoreManagement.StorageResource[]"
-
-            
-            It "Cannot mock PowerCLI Cmdlets ( https://github.com/pester/Pester/issues/803 )" {
-                $True | Should be $True
-            } -pending
-
-
-    #        It "Should accept pipeline input of type String and Convert it to a Datastore Object" {
-    #            $DS = 'Vol1'
-
-    #            { $DS | Get-VMWareOrphanedFiles -verbose } | Should not Throw
-
-    #            Assert-VerifiableMocks
-    #        } -Pending
-
-      #      It "Should accept Pipeline Input of type DataStore (VMware.VimAutomation.ViCore.Impl.V1.DatastoreManagement.VmfsDatastoreImpl)" {
-      #          $DS = Mock-NewObject -Type VMware.VimAutomation.ViCore.Impl.V1.DatastoreManagement.VmfsDatastoreImpl {}
-      #
-      #          { $DS | Get-VMWareOrphanedFiles } | Should not Throw
-      #      } -pending
-      #
-      #  
-      #      It "Should accept pipeline input of an array of Strings and convert it to a DataStore object" {
-      #          $DS = 'Vol1','Vol2'
-      #
-      #          { $DS | Get-VMWareOrphanedFiles } | Should not Throw
-      #      } -pending
-      #
-      #      It "Should write a warning if the datastore is not of type VMFS" {
-      #
-      #          Mock -Command Get-DataStore -MockWith {
-      #              $BrowserObj = New-Object -TypeName PSObject (@{
-      #                  Browser = $Null
-      #              })
-      #
-      #              New-Object -TypeName PSObject -Property (@{
-      #                  Type = 'nonVMFS'
-      #                  ExtensionData = $BrowserObj
-      #                  ID = $Null
-      #                  Name = 'Vol1'
-      #              }) 
-      #          }
-      #
-      #          $DS = 'Vol1'
-      #
-      #          $DS | Get-VMWareOrphanedFiles 3>&1 | Should Match "Get-VMWareOrphanedFiles : Skipping Vol1 as it is not a VMFS datastore."
-      #
-      #      } -Pending
-
-        }
-
-      #  Context output {
-      #
-      #      it "Should return a Custom Powershell Object" {
-      #          $DS = 'Vol1'
-      #
-      #          $DS | Get-VMWareOrphanedFiles | Should beoftype PSObject
-      #      } -pending
-      #
-      #  }
-
-    }
-
     #-------------------------------------------------------------------------------------
 
     Describe "$ModuleName : Get-VMWareDataStoreSIOC" {
--- VMWare.psm1
+++ VMWare.psm1
@@ -1,4 +1,19 @@
-﻿# -------------------------------------------------------------------------------------
+﻿#--------------------------------------------------------------------------------------
+# Module for VMWare enhancement functions
+#--------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------
+# ----- Dot source the functions in the Functions folder of this module
+# ----- Ignore any file that begins with @, this is a place holder of work in progress.
+
+Get-ChildItem -path $PSScriptRoot\Functions\*.ps1 -| where Name -notlike '@*'  Foreach { 
+    Write-Verbose "Dot Sourcing $_.FullName"
+
+    . $_.FullName 
+}
+
+
+# -------------------------------------------------------------------------------------
 # Alarm Functions
 # -------------------------------------------------------------------------------------
 
@@ -572,208 +587,6 @@ function Get-VMWareMotionHistory {
 # DataStore Functions
 # -------------------------------------------------------------------------------------
 
-
-function Get-VMWareOrphanedFile {
-
-<#
-    .SYNOPSIS
-        Find orphaned files on a datastore
-
-    .DESCRIPTION
-        This function will scan the complete content of a datastore.
-        It will then verify all registered VMs and Templates on that
-        datastore, and compare those files with the datastore list.
-        Files that are not present in a VM or Template are considered
-        orphaned
-
-    .NOTES
-        Author:  Luc Dekens
-        Edited by : Jeff Buenting
-            Date: 2018 Dec 06
-
-    .PARAMETER Datastore
-        The datastore that needs to be scanned
-
-    .EXAMPLE
-        PS> Get-VMWareOrphanedFiles -Datastore DS1
-
-    .EXAMPLE
-        PS> Get-Datastore -Name DS* | Get-VMWareOrphanedFiles
-
-    .EXAMPLE
-        Count files by datastore and used space.
-
-        Get-DataStore | Get-VMWareOrphanedFiles | group-object -Property datastore | foreach-object { New-object PSObject -Property @{ datastore=$_.name;Count=$_.count;Total=($_.group | measure-object -Property size -sum).sum/1TB} }
-
-    .Link
-        www.lucd.info/2016/09/13/orphaned-files-revisited/
-#>
-
-    [CmdletBinding(SupportsShouldProcess=$true)]
-    param(
-        [parameter(Mandatory=$true,ValueFromPipeline=$true)]
-        [PSObject[]]$Datastore
-    )
-
-
-    Begin{
-        
-        Try {
-            # ----- Search datastore sub folders parameters
-            $flags = New-Object -TypeName VMware.Vim.FileQueryFlags -ErrorAction Stop
-            $flags.FileOwner = $true
-            $flags.FileSize = $true
-            $flags.FileType = $true
-            $flags.Modification = $true
-            $qFloppy = New-Object VMware.Vim.FloppyImageFileQuery -ErrorAction Stop
-            $qFolder = New-Object VMware.Vim.FolderFileQuery -ErrorAction Stop
-            $qISO = New-Object VMware.Vim.IsoImageFileQuery -ErrorAction Stop
-            $qConfig = New-Object VMware.Vim.VmConfigFileQuery -ErrorAction Stop
-
-            $qConfig.Details = New-Object VMware.Vim.VmConfigFileQueryFlags -ErrorAction Stop                       
-            $qConfig.Details.ConfigVersion = $true
-           $qTemplate = New-Object VMware.Vim.TemplateConfigFileQuery -ErrorAction Stop
-            $qTemplate.Details = New-Object VMware.Vim.VmConfigFileQueryFlags -ErrorAction Stop
-            $qTemplate.Details.ConfigVersion = $true
-            $qDisk = New-Object VMware.Vim.VmDiskFileQuery -ErrorAction Stop
-            $qDisk.Details = New-Object VMware.Vim.VmDiskFileQueryFlags -ErrorAction Stop
-            $qDisk.Details.CapacityKB = $true
-            $qDisk.Details.DiskExtents = $true
-            $qDisk.Details.DiskType = $true
-            $qDisk.Details.HardwareVersion = $true
-            $qDisk.Details.Thin = $true
-            $qLog = New-Object VMware.Vim.VmLogFileQuery -ErrorAction Stop
-            $qRAM = New-Object VMware.Vim.VmNvramFileQuery -ErrorAction Stop
-            $qSnap = New-Object VMware.Vim.VmSnapshotFileQuery -ErrorAction Stop 
-            $searchSpec = New-Object VMware.Vim.HostDatastoreBrowserSearchSpec -ErrorAction Stop
-            $searchSpec.details = $flags
-            $searchSpec.Query = $qFloppy,$qFolder,$qISO,$qConfig,$qTemplate,$qDisk,$qLog,$qRAM,$qSnap
-            $searchSpec.sortFoldersFirst = $true
-        }
-        catch {
-            $ExceptionMessage = $_.Exception.Message
-            $ExceptionType = $_.Exception.GetType().Fullname
-            Throw "Get-VMWareOrphanedFiles : Error setting SearchDatastoreSubFolders Parameters.`n`n     $ExceptionMessage`n`n $ExceptionType"
-        }
-    }
-
-    Process{
-        foreach($ds in $Datastore) {
-
-            Try {
-                # ----- Accepts datastore names and then converts to datastore object if needed
-                if($ds.GetType().Name -eq "String"){
-                    Write-Verbose "Input is a string.  Getting Datastore"
-                    $ds = Get-Datastore -Name $ds -ErrorAction Stop
-                }
-            }
-            catch {
-                $ExceptionMessage = $_.Exception.Message
-                $ExceptionType = $_.Exception.GetType().Fullname
-                Throw "Get-VMWareOrphanedFiles : Error Getting Datastore from string input.`n`n     $ExceptionMessage`n`n $ExceptionType"
-            }
-
-            Write-Verbose "Datastore = $($DS | out-string)"
- 
-            # ----- Only shared VMFS datastore
-            if($ds.Type -eq "VMFS" -and $ds.ExtensionData.Summary.MultipleHostAccess -and $ds.Accessible){
-                Write-Verbose -Message "$(Get-Date)`t$((Get-PSCallStack)[0].Command)`tLooking at $($ds.Name)"
-  
-                # ----- Define file DB
-                $fileTab = @{}
-
-                # ----- This hash table will be used to determine which file/folder is orphaned or not. First all the files the method fins are placed in the hash table. Then the files that belong to VMs and Templates are removed. Finally some system files are removed. What is left are orphaned files.
- 
-                Try {
-                    # ----- Get datastore files
-                    $dsBrowser = Get-View -Id $ds.ExtensionData.browser
-                    $rootPath = "[" + $ds.Name + "]"
-
-            write-verbose $($DSbrowser | out-string)
-
-                    $searchResult = $dsBrowser.SearchDatastoreSubFolders($rootPath, $searchSpec) #| Sort-Object -Property {$_.FolderPath.Length}
-                }
-                catch {
-                    $ExceptionMessage = $_.Exception.Message
-                    $ExceptionType = $_.Exception.GetType().Fullname
-                    Throw "Get-VMWareOrphanedFiles : Error Getting DataStore Files.`n`n     $ExceptionMessage`n`n $ExceptionType"
-                }
-             
-                # ----- extract file object from SearchResult
-                foreach($folder in $searchResult){
-                    foreach ($file in $folder.File){
-                        Write-Verbose "Processing $($File.path)"
-
-                        $key = "$($folder.FolderPath)$(if($folder.FolderPath[-1] -eq ']'){' '})$($file.Path)"
-                        $fileTab.Add($key,$file)
-
-                        # ----- Take care of the folder entries. If a file inside a folder is encountered, the script also removes the folder itself from the hash table
-                        $folderKey = "$($folder.FolderPath.TrimEnd('/'))"       
-                        if($fileTab.ContainsKey($folderKey)){
-                            $fileTab.Remove($folderKey)
-                        }
-                    }
-                }
-
-                # ----- Remove files from the list belonging to existing VMs
-                Get-VM -Datastore $ds | Foreach {
-                    write-verbose "VM = $($_ | out-string)"
-                    $_.ExtensionData.LayoutEx.File | Foreach {
-                        if($fileTab.ContainsKey($_.Name)){
-                            $fileTab.Remove($_.Name)
-                        }
-                    }
-                }
- 
-                # ----- Remove files from the list belonging to existing Templates
-                Get-Template -ErrorAction Stop | where {$_.DatastoreIdList -contains $ds.Id} | %{
-                    $_.ExtensionData.LayoutEx.File | %{
-                        if($fileTab.ContainsKey($_.Name)){
-                            $fileTab.Remove($_.Name)
-                        }
-                    }
-                }
-
-                # ----- Remove system files & folders from list.  Any file in a folder that begins with a . ro VMKDump
-                $systemFiles = $fileTab.Keys | where{$_ -match "] \.|vmkdump"}
-                $systemFiles | Foreach {
-                    $fileTab.Remove($_)
-                }
- 
-                # ----- Organise remaining files.  These are the orphaned files
-                if($fileTab.Count){
-                      $fileTab.GetEnumerator() | Foreach {
-                            $obj = [ordered]@{
-                                  Name = $_.Value.Path
-                                  Folder = $_.Name
-                                  Size = $_.Value.FileSize
-                                  CapacityKB = $_.Value.CapacityKb
-                                  Modification = $_.Value.Modification
-                                  Owner = $_.Value.Owner
-                                  Thin = $_.Value.Thin
-                                  Extents = $_.Value.DiskExtents -join ','
-                                  DiskType = $_.Value.DiskType
-                                  HWVersion = $_.Value.HardwareVersion
-                                  DataStore = $DS.Name
-                            }
- 
-                            New-Object -type PSObject -Property $obj
-                      }
- 
-                      Write-Verbose -Message "$(Get-Date)`t$((Get-PSCallStack)[0].Command)`tFound orphaned files on $($ds.Name)!"
-                }
-                else{
-                     Write-Verbose -Message "$(Get-Date)`t$((Get-PSCallStack)[0].Command)`tNo orphaned files found on $($ds.Name)."
-                }
-            }
-            Else {
-                Write-Warning "Get-VMWareOrphanedFiles : Skipping $($DS.Name) as it is not a VMFS datastore."
-            }
-        }
-    }
-}
-
-# -------------------------------------------------------------------------------------
 
 function Get-VMWareDataStoreSIOC {
     

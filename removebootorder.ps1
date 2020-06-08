29b0d688d99384f6ccaccfad607daa101ce4cc56
jeff <jeffbuenting@gmail.com>
2020-06-08 07:52:23 -0400

    Signed-off-by: jeff <jeffbuenting@gmail.com>

---
+++ removebootorder.ps1
@@ -0,0 +1,88 @@
+﻿#    # ----- remove temp boot order
+#        # ----- Gather VM info we will need
+#        $VM = Get-VM -Name $VMName
+#
+#        $VMXPath = $VM.ExtensionData.Config.Files.VmPathName
+#        $VMXDS = Get-Datastore $VMXPath.split(' ')[0].trim('[',']')
+#        $VMXName = $VMXPath.split(' ')[1].Replace( '/','\')
+#        $Folder = $VM.Folder
+#
+#        # ----- If VM is in resouce pool use that otherwise just use the cluster as the resource
+#        if ( $($VM.ResourcePool) ) {
+#            $ResourcePool = $VM.ResourcePool
+#        }
+#        Else {
+#            $ResourcePool = (Get-Cluster -VM $VM).Name
+#        }
+#
+#        # ----- It seems that when you change the Bootoptions via PowerCLI, it also changes the BIOS order.  And removing the BootOrder from the VMX does not set the order back.  SO setting it with Powerclie and then clearing
+#
+#        $spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+#
+#        $BootOptions = New-Object VMware.Vim.VirtualMachineBootOptions
+#
+#        $BootableCDRom = New-Object -Type VMware.Vim.VirtualMachineBootOptionsBootableCdromDevice
+#
+#        $HDiskDeviceName = "Hard disk 1"
+#        $HDiskDeviceKey = ($vm.ExtensionData.Config.Hardware.Device | ?{$_.DeviceInfo.Label -eq $HDiskDeviceName}).Key
+#        $BootableHDisk = New-Object -TypeName VMware.Vim.VirtualMachineBootOptionsBootableDiskDevice -Property @{"DeviceKey" = $HDiskDeviceKey}
+#
+#        $BootOrder = $BootableCDRom
+#
+#        $BootOptions.BootOrder = $BootableHDisk,$BootOrder
+#
+#        $Spec.BootOptions = $BootOptions
+#
+#        $VM.ExtensionData.reconfigvm( $Spec )
+#
+#        # ----- Remove and Reregister so VMX changes happen
+#        Remove-Inventory -Item $VM -Confirm:$False
+#
+#        # ----- Register VM (use clustername as the default resourcepool)
+#        $VM = New-VM -VMFilePath $VMXPath -Location $Folder -ResourcePool $ResourcePool
+#
+#        # ----- Apparently the settings don't change unless the VM boots.  Booting and shuting down
+#        Start-VM -VM $VM 
+#        Wait-Tools -vm $VM
+#
+#        Shutdown-VMGuest -vm $VM -Confirm:$False 
+#
+#        $VM = Get-VM -Name $VMName
+#
+#        # ----- Wait for VM to be powered off.
+#        Write-Output "Waiting until the VM is in a PoweredOff State prior to changing boot order"
+#        while ( $VM.PowerState -ne 'PoweredOff' ) {
+#            Start-Sleep -s 5
+#            Write-Output "Powerstate = $($VM.Powerstate)"
+#            $VM = Get-VM -Name $VMName
+#        }
+#
+#
+#
+#
+#        # ----- Clearing Bios.bootORder from VMX
+#
+#        # ----- Editing the VMX to be safe unregister VM
+#        Remove-Inventory -Item $VM -Confirm:$False
+#
+#        # ----- Copy locally to edit and rename as backup
+#        Copy-DatastoreItem  -Item "$($VMXDS.DatastoreBrowserPath)\$VMXName" -Destination c:\temp\$($VMName).vmx.old
+#
+#        # ----- Renaming to .old as backup
+#        #Rename-Item c:\temp\$($VMName).vmx -NewName c:\temp\$($VMName).vmx.old
+#
+#        Get-Content -Path c:\temp\$($VMName).vmx.old | Select-String -Pattern bios.bootOrder -NotMatch | Set-Content -Path c:\temp\$($VMName).vmx
+#
+#
+#        # ----- back to datastore and register VM
+#        Copy-DatastoreItem -Item c:\temp\$($VMName).vmx -Destination "$($VMXDS.DatastoreBrowserPath)\$VMXName"
+#
+#        # ----- Register VM (use clustername as the default resourcepool)
+#        $VM = New-VM -VMFilePath $VMXPath -Location $Folder -ResourcePool $ResourcePool 
+#
+#   #     $spec = New-Object VMware.Vim.VirtualMachineConfigSpec 
+#   #     $Spec.BootOPtions = New-Object VMware.Vim.VirtualMachineBootOptions 
+#   #     $SPec.BootOptions.BootOrder = $Null
+#   #
+#   #     ## reconfig the VM to use the spec with the new BootOrder
+#   #     $vm.ExtensionData.ReconfigVM_Task($spec)
---
+++ setbootorder.ps1
@@ -0,0 +1,70 @@
+﻿#    # ----- VM must be Powered off to change boot order
+#    Write-Log -Path "$LogPath\$($VMName).log"  -Message "Shutting down the VM" -Verbose:$IsVerbose
+#    Shutdown-VMGuest -VM $VM -Confirm:$False
+#
+#    $VM = Get-VM -Name $VMName
+#
+#    # ----- Wait for VM to be powered off.
+#    Write-Log -Path "$LogPath\$($VMName).log"  -Message "Waiting until the VM is in a PoweredOff State prior to changing boot order" -Verbose:$IsVerbose
+#    while ( $VM.PowerState -ne 'PoweredOff' ) {
+#        Start-Sleep -s 5
+#        Write-Output "Powerstate = $($VM.Powerstate)"
+#        $VM = Get-VM -Name $VMName
+#    }
+#
+#    # ----- Configure VM to Boot from WINPEUIFIConvertion ISO
+#        Write-Log -Path "$LogPath\$($VMName).log"  -Message "Setting CDRom as only boot option" -Verbose:$IsVerbose
+#
+#        # ----- Capture info needed to register vm
+#        $VMXPath = $VM.ExtensionData.Config.Files.VmPathName
+#        $Folder = $VM.Folder
+#        if ( $($VM.ResourcePool) ) {
+#            $ResourcePool = $VM.ResourcePool
+#        }
+#        Else {
+#            $ResourcePool = (Get-Cluster -VM $VM).Name
+#        }
+#
+#
+#        # ----- Set CDROM as first boot
+#        $spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+#
+#        $BootOptions = New-Object VMware.Vim.VirtualMachineBootOptions
+#
+#        $BootableCDRom = New-Object -Type VMware.Vim.VirtualMachineBootOptionsBootableCdromDevice
+#
+#        #$HDiskDeviceName = "Hard disk 1"
+#        #$HDiskDeviceKey = ($vm.ExtensionData.Config.Hardware.Device | ?{$_.DeviceInfo.Label -eq $HDiskDeviceName}).Key
+#        #$BootableHDisk = New-Object -TypeName VMware.Vim.VirtualMachineBootOptionsBootableDiskDevice -Property @{"DeviceKey" = $HDiskDeviceKey}
+#
+#        $BootOrder = $BootableCDRom
+#
+#        $BootOptions.BootOrder = $BootOrder
+#
+#        $Spec.BootOptions = $BootOptions
+#
+#        $VM.ExtensionData.reconfigvm( $Spec )
+#
+#        # ----- Remove and Reregister so VMX changes happen
+#        Remove-Inventory -Item $VM -Confirm:$False
+#
+#        # ----- Register VM (use clustername as the default resourcepool)
+#        $VM = New-VM -VMFilePath $VMXPath -Location $Folder -ResourcePool $ResourcePool
+#
+#
+#
+#
+#
+#
+#        #    $spec = New-Object VMware.Vim.VirtualMachineConfigSpec 
+#        #    $Spec.BootOPtions = New-Object VMware.Vim.VirtualMachineBootOptions 
+#        #    $SPec.BootOptions.BootOrder = New-Object -Type VMware.Vim.VirtualMachineBootOptionsBootableCdromDevice
+#        #
+#        #    ## reconfig the VM to use the spec with the new BootOrder
+#        #    $vm.ExtensionData.ReconfigVM_Task($spec)
+#
+#    # ----- so I am having problem imediately starting the VM.  So pausing for x Seconds
+#    Start-Sleep -Seconds 30
+#
+#    Write-Log -Path "$LogPath\$($VMName).log"  -Message "Booting to WINPE to performing the magic" -Verbose:$IsVerbose
+#    Start-VM -vm $VM 

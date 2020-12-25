dfd8e9979177e59c16adccb0bc8f522ea2875f80
Jeff Buenting <buenting_jeffrey@bah.com>
2020-12-25 17:14:27 -0500

    added Timeout to Wait-VMState

--- Functions/Wait-VMState.ps1
+++ Functions/Wait-VMState.ps1
@@ -7,18 +7,24 @@
 
         [Parameter (Mandatory = $True) ]
         [ValidateSet ('PoweredON','PoweredOff')]
-        [String]$State
+        [String]$State,
 
-        
+        [int]$TimeoutSec = 300
     )
+
+    $timer = [Diagnostics.Stopwatch]::StartNew()
 
     Do {
         Write-Verbose "Waiting for VM to $State ..."
-        Start-Sleep -Seconds 30
+        Start-Sleep -Seconds 15
         
         $VM = Get-VM -Name $VM.Name
 
-    } While ( $VM.PowerState -ne $State )
+    } Until ( ( $VM.PowerState -ne $State ) -and ($timer.Elapsed.TotalSeconds -lt $TimeoutSec) )
+
+    if ( $Timer.Elapsed.TotalSeconds -ge $TimeoutSec ) {
+        Throw "Wait-VMState : Error waiting for VM to $State"
+    }
 
     Write-Verbose $State
 }

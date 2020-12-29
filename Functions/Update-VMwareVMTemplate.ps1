45ffc1f76708fb213c09445bb5082537ab2dabad
Jeff Buenting <jeffbuenting@users.noreply.github.com>
2020-12-29 16:00:13 -0500

    removed WU code.  now it waits for auto updates to happen

--- Functions/Update-VMwareVMTemplate.ps1
+++ Functions/Update-VMwareVMTemplate.ps1
@@ -8,24 +8,20 @@
         Installs patches.  Currently only works with WIndows patches.
 
     .PARAMETER Template
-        Template object or Template name.
+        Template object.  Use Get-Template
 
-    .PARAMETER GuestCredential
-        Admin credentials for the templates guest OS.
-
-    .EXAMPLE
-        Install Updates on template given a name.
-
-        Update-VMwareVMTemplate -Template 'Windows2019' -GuestCredential $Cred
+    .PARAMETER WaitHours
+        Number of hours to wait while patches are installed before converting back to templates.
 
     .EXAMPLE
-        Install Updates on a template 
+        Install Updates on templates. and wait 24 hours to install patches.
 
-        $Template = Get-Template 'Windows2019'
-        Update-VMwareVMTemplate -Template $Template -GuestCredential $Cred
+        Get-Template | Update-VMWareVMTemplate -verbose
 
-    .LINK
-        https://github.com/MicrosoftDocs/windows-powershell-docs
+    .EXAMPLE
+        Install Updates on templates. and wait 12 hours to install patches.
+
+        Get-Template | Update-VMWareVMTemplate -WaitHours 12 -verbose
 
     .NOTES
         Author : Jeff Buenting
@@ -34,102 +30,71 @@
 
     [CmdletBinding()]
     Param (
-        [Parameter (Mandatory = $True) ]
-        [PSObject]$Template,
+        [Parameter (Mandatory = $True,ValueFromPipeline = $True) ]
+        [VMware.VimAutomation.ViCore.Impl.V1.Inventory.TemplateImpl[]]$Template,
 
-        [Parameter (Mandatory = $True) ]
-        [PSCredential]$GuestCredential,
-        
-        [Parameter (Mandatory = $True) ]
-        [String]$RemoteLog,
-
-        [Int]$TimeoutSec = 3600
+        [Decimal]$WaitHours = 24
     )
     
-    # ----- Get the template if it $Template is not a template
-    if ( $Template.GetType().Name -ne 'TemplateImpl' ) {
+    Begin {
+        $VMTemplates = @()
+    }
+
+    Process {
+        # ----- Gather all Templates to run in parallel
+        Foreach ( $T in $Template ) {
+            $VMTemplates += $T
+        }
+
+    }
+
+    End {
         Try {
-            Write-Verbose "Retrieving Template $Template"
-            $Template = Get-Template -Name $Template -ErrorAction Stop
+            # ----- Convert Template to VM
+            Set-Template -Template $VMTemplates -ToVM | Write-Verbose
+ 
+            $VM = Get-VM -Name $VMTemplates.Name 
+ 
+            Start-VM -VM $VM | write-Verbose
+
+            Write-Verbose "VMs started, waiting for VMware Tools"
+            Wait-Tools -VM $VM -TimeoutSeconds 300 -ErrorAction Stop | write-Verbose
         }
         Catch {
             $ExceptionMessage = $_.Exception.Message
             $ExceptionType = $_.Exception.GetType().Fullname
-            Throw "Update-VMwareVMTemplate : Template does not exist.`n`n     $ExceptionMessage`n`n $ExceptionType"
+            Throw "Update-VMwareVMTemplate : Error converting to VM or Starting VM.`n`n     $ExceptionMessage`n`n $ExceptionType"
         }
-    }
+ 
+        # ----- Wait X amount of time for Patches to be applied
+        Write-Verbose "Waiting for $($WaitHours*60*60) Seconds for Patching"
+        Start-Sleep -Seconds ($WaitHours * 60 * 60)
+ 
+        Try {
+            # ----- problems restarting VM.  Turns out you have to get the VM object again.
+            $VM = Get-VM -Name $VMTemplates.Name
 
-    # ----- Convert Template to VM
-    Write-Verbose "Converting $($Template.Name) to VM"
-
-    Set-Template -Template $Template -ToVM | Write-Verbose
-
-    $VM = Get-VM -Name $Template.Name 
-
-    Start-VM -VM $VM | write-Verbose
-
-    Wait-VMState -VM $VM -State PoweredON
-
-    # ----- Scan and install Updates
-# ----- Is the PSWindowsUpdate module there?
-    $CreateScript =  @"
-        `$UpdateCMD = @'
-            Out-File -FilePath $RemotePath\TemplateUpdate.log -InputObject "--------------------------" -Append
-
-
-
-            if ( -Not ( Get-Module -ListAvailable -Name PSWindowsUPdate ) ) {
-                Out-File -FilePath $RemotePath\TemplateUpdate.log -InputObject "`$(Get-Date -Format G) - Start-OSUpdate : Error : PSWindowsUpdate is required." -Append
-                Throw "Start-OSUpdate : PSWindowsUpdate is required."
-            }
-            Else {
-                Out-File -FilePath $RemotePath\TemplateUpdate.log -InputObject "`$(Get-Date -Format G) - Importing PSWIndowsUpdate Module." -Append
-                Import-module PSWindowsUpdate
+            # ----- Shut down the VM
+            $VM | foreach { 
+                $_.Name
+                '+'
+                Shutdown-VMGuest -VM $_ -Confirm:$False | Write-Verbose
+                "-----"
             }
 
-            Try {
-                `$Updates = Get-WUList -ErrorAction Stop
-                Out-File -FilePath $RemotePath\TemplateUpdate.log -InputObject "`$(Get-Date -Format G) - ``n`$(`$Updates | Out-string)." -Append
-                `$Updates | Install-WindowsUpdate -AutoReboot -Confirm $False -ErrorAction Stop
-            }
-            Catch {
-                `$ExceptionMessage = `$_.Exception.Message
-                `$ExceptionType = `$_.Exception.GetType().Fullname
-                Out-File -FilePath $RemotePath\TemplateUpdate.log -InputObject "`$(Get-Date -Format G) - Start-OSUpdate : Error : Getting or Installing Updates.``n``n     `$ExceptionMessage``n``n `$ExceptionType" -Append
-            }
-'@
-        `$UpdateCmd | out-file $RemotePath\Start-OSUpdate.ps1 -Append
-"@
+            Wait-VMState -VM $VM -State PoweredOff -ErrorAction Stop 
 
-Invoke-VMScript -VM $VM -ScriptText $CreateScript -GuestCredential $GuestCredential
+            # ----- Again issues with working with the VM unless we reget.
+            $VM = Get-VM -Name $VMTemplates.Name
 
-
-    $CMD = @"
-        Start-Process C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Verb Runas -argumentList "-ExecutionPolicy Bypass -File $RemotePath\Start-OSUpdate.ps1"
-"@
-
-    Invoke-VMScript -VM $VM -ScriptText $Cmd -GuestCredential $GuestCredential
-
-    Try {
-        # ----- Restart to finish installs
-        Restart-VMGuest -VM $VM -ErrorAction Stop | write-Verbose
-
-        Wait-VMState -VM $VM -State PoweredON -ErrorAction Stop 
-    
-        # ----- Pause after restart to let things settle
-        Start-Sleep -Seconds 300
-
-        # ----- Shut down the VM
-        Shutdown-VMGuest -vm $VM -Confirm:$False -ErrorAction Stop | Write-Verbose
-
-        Wait-VMState -VM $VM -State PoweredOff -ErrorAction Stop 
-
-        # ----- Convert to Template
-        Set-VM -VM $VM -ToTemplate -Confirm:$False -ErrorAction Stop | Write-Verbose
-    }
-    Catch {
+            # ----- Convert to Template
+            Write-Verbose "Converting to Template"
+            $VM | Set-VM -ToTemplate -Confirm:$False | Write-Verbose
+        }
+        Catch {
             $ExceptionMessage = $_.Exception.Message
             $ExceptionType = $_.Exception.GetType().Fullname
-            Throw "Update-VMwareVMTemplate : There was a problem shutting down VM and Converting it to a template.`n`n     $ExceptionMessage`n`n $ExceptionType"
+            Throw "Update-VMwareVMTemplate : Error shutting down VM or converting to Template.`n`n     $ExceptionMessage`n`n $ExceptionType"
         }
+    }
 }
